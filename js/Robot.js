1f833102d3e0ea7e3cb7de188f36202817ed41c9
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-22 14:27:28 -0600

    created Robot constructor and added 3 types ATV, BiPedal and Drone

--- js/Robot.js
+++ js/Robot.js
@@ -1,51 +1,112 @@
-var battleDome = BattleDome || {};
-battleDome.fighters = {};
 
 
-
-battleDome.players.robot = function(name) {
-  this.species = null;
-  this.class = null;
-  this.weapon = null;
-
-  this.robotName = name || "Bot with no name";
-  this.health = Math.floor(Math.random() * 40 + 50);
-  this.flight = false;
-  this.strength = 90;
+function Robot () {
+    this.type = 'Robot';
+    this.flight = false;
+    this.robotName = name || "Bot with no name";
+    this.health = Math.floor(Math.random() * 40 + 50);
+    this.strength = 90;
 
 };
 
-
-
-
-battleDome.players.drone = function() {
-
-  this.flight = true;
-  this.species = "Drone";
-  this.weapon = "Missles"
-
-  // this.allowedClasses = ["Warrior", "Berserker", "Valkyrie",];
+function Drone (name) {
+   this.type = 'Drone';
+   this.name = name;
+   this.flight = true;
+   this.weapon = "Missles"
 };
-battleDome.fighters.drone.prototype = new battleDome.fighters.robot();
+
+Drone.prototype = new Robot();
+var newDrone = new Drone("Bobby")
+console.log(newDrone.weapon, newDrone.name, newDrone.strength, newDrone.health)
 
 
-battleDome.players.atv = function() {
-
-  this.flight = false;
-  this.species = "ATV";
-  this.weapon = "Machine Gun"
-
-  // this.allowedClasses = ["Warrior", "Berserker", "Valkyrie",];
+function Bipedal (name) {
+   this.type = 'Bipedal';
+   this.name = name;
+   this.flight = false;
+   this.weapon = "Many, Many Slaps to the Face"
 };
-battleDome.fighters.drone.prototype = new battleDome.fighters.robot();
+
+Bipedal.prototype = new Robot();
+
+var bingo = new Bipedal("Bibbity")
+
+console.log(bingo.name + " will defeat you with " + bingo.weapon + ". Unless, you rip down his health, which is " + bingo.health)
 
 
-battleDome.players.bipedal = function() {
 
-  this.flight = false;
-  this.species = "BiPedal";
-  this.weapon = "Power Punches"
-
-  // this.allowedClasses = ["Warrior", "Berserker", "Valkyrie",];
+function ATV (name) {
+   this.type = 'ATV';
+   this.name = name;
+   this.flight = false;
+   this.weapon = "Bombs"
 };
-battleDome.fighters.drone.prototype = new battleDome.fighters.robot();
+
+ATV.prototype = new Robot();
+
+var activity = new ATV("Arron");
+
+console.log("this is the ATV: ", activity.name, activity.health)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var BattleDome = BattleDome || {};
+// BattleDome.fighters = {};
+
+
+
+// BattleDome.fighters.robot = function(name) {
+//   this.species = null;
+//   this.class = null;
+//   this.weapon = null;
+//   this.flight = false;
+//   this.robotName = name || "Bot with no name";
+//   this.health = Math.floor(Math.random() * 40 + 50);
+//   this.strength = 90;
+
+// };
+
+
+
+
+// BattleDome.fighters.drone = function() {
+//   this.flight = true;
+//   this.species = "Drone";
+//   this.weapon = "Missles"
+
+
+// };
+// BattleDome.fighters.drone.prototype = new BattleDome.fighters.robot();
+
+// console.log(drone.weapon)
+// BattleDome.fighters.atv = function() {
+//   this.species = "ATV";
+//   this.weapon = "Machine Gun"
+
+//   // this.allowedClasses = ["Warrior", "Berserker", "Valkyrie",];
+// };
+// BattleDome.fighters.atv.prototype = new BattleDome.fighters.robot();
+// // ---------------------------------------------------------------------------
+// bipedal.prototype = new robot();
+
+// BattleDome.fighters.bipedal = function() {
+//   this.species = "BiPedal";
+//   this.weapon = "Power Punches"
+
+//   // this.allowedClasses = ["Warrior", "Berserker", "Valkyrie",];
+// };
+// BattleDome.fighters.bipedal.prototype = new BattleDome.fighters.robot();
+
+
+// console.log(BattleDome.fighters.bipedal.weapon)
